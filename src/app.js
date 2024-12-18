8f1f79a389054d0eba3dda16ddd02f411e1c422c
Oerlinker <oerlinker@gmail.com>
2024-12-18 02:01:48 -0400

    olvide instalar cors y permitir que admita todos los metodos de cualquier origen.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 const app = express();
 
 
 
 app.use(express.json());
-const routes = require('./routes');
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use('/api', routes);
 
 
